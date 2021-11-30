11b74fec40419021ca06c84ea58954678fa183c9
Suzan M <suzanmanasreh@gmail.com>
2021-11-29 21:23:55 -0500

    edit people page

--- pages/people.js
+++ pages/people.js
@@ -16,8 +16,19 @@ function People() {
             People
           </h1>
           <hr />
-          <p className="my-5">Insert people's pictures here</p>
-          <img src={"/professor.jpeg"} className="border-2" />
+          <div className="flex content-start my-5">
+            {/* <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div>4</div>
+            <div>5</div> */}
+            <img src={"/professor.jpeg"} className="border-2" />
+            <>&emsp;</>
+            <p className="relative z-0">Mohammed Aledhari</p>
+            {/* <p className="relative flex items-center z-10">
+              Computer Science Professor
+            </p> */}
+          </div>
         </div>
       </div>
       <Footer />
