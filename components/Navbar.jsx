c0d62519ad68539ef2a00877f054effe27c40a84
Suzan M <suzanmanasreh@gmail.com>
2021-11-29 20:37:48 -0500

    fix css

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,7 +9,7 @@ export const Navbar = () => {
   };
   return (
     <>
-      <nav className="flex items-center flex-wrap bg-gradient-to-r from-blue-900 to-green-700 p-4">
+      <nav className="flex items-center flex-wrap bg-gradient-to-r from-blue-900 to-green-700 p-4 absolute top-0 w-full">
         <Link href="/">
           <a className="inline-flex items-center p-2 mr-4 ml-5">
             {/* <svg
@@ -19,7 +19,7 @@ export const Navbar = () => {
             >
               <path d="M12.001 4.8c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624C13.666 10.618 15.027 12 18.001 12c3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C16.337 6.182 14.976 4.8 12.001 4.8zm-6 7.2c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624 1.177 1.194 2.538 2.576 5.512 2.576 3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C10.337 13.382 8.976 12 6.001 12z" />
             </svg> */}
-            <span className="text-2xl text-gray-50 font-bold uppercase tracking-wide">
+            <span className="text-2xl text-gray-50 font-bold uppercase tracking-wide ml-12">
               KSU BioInformatics Lab
             </span>
           </a>
