5146fe96c5d66b5c5ee840bd849cbeb8cde4c16e
Suzan M <suzanmanasreh@gmail.com>
2021-12-05 18:10:30 -0500

    make location content responsive

--- pages/contact.js
+++ pages/contact.js
@@ -16,16 +16,16 @@ function Contact() {
             Location of the Lab
           </h1>
           <hr />
-          <div className="overflow-hidden flex content-start text-base grid grid-cols-2">
+          <div className="overflow-hidden md:flex content-start text-base">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3310.157545647305!2d-84.52191994870782!3d33.93707583114171!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88f5115510312027%3A0x81ce76f9703afb9c!2sKennesaw%20State%20University%20-%20Marietta%20Campus!5e0!3m2!1sen!2sus!4v1638740161753!5m2!1sen!2sus"
               width="480"
               height="360"
               allowfullscreen=""
               loading="lazy"
-              className="border-2 my-8"
+              className="border-2 mt-8"
             ></iframe>
-            <div className="py-8">
+            <div className="py-8 pl-4">
               <p className="py-1">Kennesaw State University, Marietta Campus</p>
               <p className="py-1">
                 College of Computing and Software Engineering
