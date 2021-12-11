385ce88ab6c734e3fcc3f060e227550ddc8e4bee
Suzan M <suzanmanasreh@gmail.com>
2021-12-11 00:20:13 -0500

    make news post array

--- pages/news.js
+++ pages/news.js
@@ -7,10 +7,26 @@ import Image from "next/image";
 
 {
   /*
-    TODO: make news posts array so the posts aren't hardcoded anymore
-    TODO: resize images to all thes same size or figure out how to set the size all the same in the code
+    TODO: add real news posts
   */
 }
+
+const newsPosts = [
+  {
+    img: require("../assets/genetics.jpg"),
+    title: "News Post Title",
+    date: "Caption",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    img: require("../assets/rename.jpg"),
+    title: "News Post Title",
+    date: "Caption",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+];
 
 function News() {
   return (
@@ -25,59 +41,31 @@ function News() {
           News
         </h1>
         <hr />
-        <div className="md:flex my-9 h-full">
-          <Image
-            src={genetics}
-            objectFit="cover"
-            width={900}
-            height={500}
-            className=""
-          />
-          <div className="md:ml-4 ">
-            <h1 className="font-semibold text-blue-800 text-xl">
-              News Post Title
-            </h1>
-            <p className="my-1 font-normal italic text-gray-400">
-              December 7, 2021
-            </p>
-            <p className="my-2 text-base font-normal text-gray-900">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-          </div>
-        </div>
-        <hr className="" />
-        <div className="md:flex my-9 h-full">
-          <Image
-            src={rename}
-            objectFit="cover"
-            width={900}
-            height={500}
-            className=""
-          />
-          <div className="md:ml-4">
-            <h1 className="font-semibold text-blue-800 text-xl">
-              News Post Title
-            </h1>
-            <p className="my-1 font-normal italic text-gray-400">
-              December 7, 2021
-            </p>
-            <p className="my-2 text-base font-normal text-gray-900">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-          </div>
-        </div>
+        {newsPosts.map((newsPost, index) => (
+          <>
+            <div className="md:flex my-9 h-full" key={index}>
+              <Image
+                src={newsPost.img}
+                objectFit="cover"
+                width={900}
+                height={500}
+                className=""
+              />
+              <div className="md:ml-4 ">
+                <h1 className="font-semibold text-blue-800 text-xl">
+                  {newsPost.title}
+                </h1>
+                <p className="my-1 font-normal italic text-gray-400">
+                  {newsPost.date}
+                </p>
+                <p className="my-2 text-base font-normal text-gray-900">
+                  {newsPost.content}
+                </p>
+              </div>
+            </div>
+            <hr />
+          </>
+        ))}
       </div>
       <Footer />
     </div>
